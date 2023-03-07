251d8ae2aa0d023b6d490de2bc807a792c52a32f
leaf-lxh <meleaf@qq.com>
2023-03-07 22:35:32 +0800

    fixed flex rules

--- renderer/components/HeiMusicMainLayout.tsx
+++ renderer/components/HeiMusicMainLayout.tsx
@@ -34,7 +34,7 @@ import { HeiMusicThemeContext } from '../lib/HeiMusicThemeProvider';
 import { useTheme } from '@mui/styles';
 import OnCloseDialog from './OnCloseDialog';
 
-function HeiMusicMainLayout({children}) {
+function HeiMusicMainLayout({ children }) {
     const theme = useTheme();
     const heimusicThemeContext = React.useContext(HeiMusicThemeContext)
     const [dialogOpen, setDialogOpen] = React.useState(false)
@@ -43,20 +43,20 @@ function HeiMusicMainLayout({children}) {
         setDialogOpen(true);
         // window.electronAPI.windowManagement.close()
     }
- 
+
     return (
         <Box sx={{ width: "calc(100vw)", height: "calc(100vh)", display: "flex", flexDirection: 'column', background: `url(${heimusicThemeContext.backgroundUrl})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
             <LoginDialog open={loginDialogOpen} onClose={() => setLoginDialogOpen(false)} />
-            <OnCloseDialog open={dialogOpen} onClose={() =>  setDialogOpen(false) } />
+            <OnCloseDialog open={dialogOpen} onClose={() => setDialogOpen(false)} />
             {/* Logo 与 系统标题栏 */}
-            <Box sx={{ height: 64, width: "100%", display: "flex", WebkitAppRegion: "drag", userSelect: "none", }}>
-                <Box sx={{ width: "196px", height: "100%", background: theme.palette.pannelBackground.main, display:"flex"}}  >
-                    <Box sx={{margin:"auto auto auto 12px", display:"flex"}}>
+            <Box sx={{ height: 64, flex:"0 0 auto", width: "100%", display: "flex", WebkitAppRegion: "drag", userSelect: "none", }}>
+                <Box sx={{ width: "196px", height: "100%", background: theme.palette.pannelBackground.main, display: "flex" }}  >
+                    <Box sx={{ margin: "auto auto auto 12px", display: "flex" }}>
                         <Avatar src='/images/logo.jpg'></Avatar>
                         <Typography sx={{ margin: "auto 0 auto 6px" }} variant="h6">HeiMusic!</Typography>
                     </Box>
                 </Box>
-                <Box sx={{flexGrow: 1, display: "flex",background: theme.palette.pannelBackground.light }}>
+                <Box sx={{ flexGrow: 1, display: "flex", background: theme.palette.pannelBackground.light }}>
                     <Box sx={{ margin: "auto 5px auto auto", WebkitAppRegion: "no-drag", }}>
                         <Button onClick={() => setLoginDialogOpen(true)} size="small" >点击登录</Button>
                     </Box>
@@ -64,29 +64,29 @@ function HeiMusicMainLayout({children}) {
                         <HeiMusicThemeContext.Consumer>
                             {
                                 context => {
-                                    return <IconButton onClick={() => context.setMode(context.mode === "light" ? "customizedDarkMode" : "light")}><Skin fontSize='small' /></IconButton>
+                                    return <IconButton  sx={{ color: theme.palette.text.primary }} onClick={() => context.setMode(context.mode === "light" ? "customizedDarkMode" : "light")}><Skin fontSize='small' /></IconButton>
                                 }
                             }
                         </HeiMusicThemeContext.Consumer>
 
-                        <IconButton><MenuIcon fontSize='small' /></IconButton>
+                        <IconButton sx={{ color: theme.palette.text.primary }}><MenuIcon fontSize='small' /></IconButton>
                     </Box>
                     <Box sx={{ width: 2, height: 18, background: "gray", margin: "auto 6px auto 6px" }}></Box>
                     <Box sx={{ margin: "auto 0 auto 0", WebkitAppRegion: "no-drag" }}>
-                        <IconButton ><Remove /> </IconButton>
-                        <IconButton ><Crop75OutlinedIcon /> </IconButton>
-                        <IconButton onClick={onCloseClicked}><Close /></IconButton>
+                        <IconButton sx={{ color: theme.palette.text.primary }}><Remove /> </IconButton>
+                        <IconButton sx={{ color: theme.palette.text.primary }} ><Crop75OutlinedIcon /> </IconButton>
+                        <IconButton sx={{ color: theme.palette.text.primary }} onClick={onCloseClicked}><Close /></IconButton>
                     </Box>
                 </Box>
             </Box>
             {/* 左侧面板 与 右侧 */}
-            <Box sx={{ flexGrow: 1, width: "100%", display: "flex" }}>
-                <LeftPannel sx={{ width: "196px", height:"calc(100vh - 64px)", overflowY:"auto", display: "flex", flexDirection: "column", background: theme.palette.pannelBackground.main }} />
+            <Box sx={{display: "flex", height: "100%" }}>
+                <LeftPannel sx={{ width: "196px", flex:"0 0 auto", overflowY: "auto", display: "flex", flexDirection: "column", background: theme.palette.pannelBackground.main }} />
                 {/* 上部视窗 与 下部播放器 */}
-                <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1 ,background: theme.palette.pannelBackground.light }}>
+                <Box sx={{ overflow:"hidden", width:"100%",  display: "flex", flexDirection: "column", background: theme.palette.pannelBackground.light }}>
                     {/* <MusicAlbum sx={{ flexGrow: 1 }} /> */}
-                    <Box sx={{flexGrow: 1}}>{children}</Box>
-                    <MusicControlPannel sx={{ height: "76px"}} />
+                    <Box sx={{ flexGrow: 1, flexShrink: 1, maxHeight: "calc(100vh - 64px - 76px)" }}>{children}</Box>
+                    <MusicControlPannel sx={{ height: "76px", flexShrink: 0 }} />
                 </Box>
             </Box>
 
--- renderer/pages/album/[id].tsx
+++ renderer/pages/album/[id].tsx
@@ -1,5 +1,5 @@
 import Box from '@mui/material/Box'
-import { BoxProps } from "@mui/material"
+import { BoxProps, Typography } from "@mui/material"
 
 import * as React from 'react';
 import Table from '@mui/material/Table';
@@ -17,123 +17,79 @@ import { useRouter } from 'next/router';
 
 
 interface Data {
-    calories: number;
-    carbs: number;
-    dessert: string;
-    fat: number;
-    id: number;
-    protein: number;
+    title: string,
+    artists: string[],
+    duration: string,
 }
 
 interface ColumnData {
-    dataKey: keyof Data;
-    label: string;
-    numeric?: boolean;
-    width: number;
+    key: string,
+    label: string,
+    width: number,
 }
 
-type Sample = [string, number, number, number, number];
-
-const sample: readonly Sample[] = [
-    ['Frozen yoghurt', 159, 6.0, 24, 4.0],
-    ['Ice cream sandwich', 237, 9.0, 37, 4.3],
-    ['Eclair', 262, 16.0, 24, 6.0],
-    ['Cupcake', 305, 3.7, 67, 4.3],
-    ['Gingerbread', 356, 16.0, 49, 3.9],
-];
-
-function createData(
-    id: number,
-    dessert: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
-): Data {
-    return { id, dessert, calories, fat, carbs, protein };
-}
+const rows: Data[] = Array.from({ length: 2000 }, (_, index) => {
+    return { title: "歌曲名称" + index, artists: ["艺术家a", "艺术家b"], duration: "1:59" };
+});
 
 const columns: ColumnData[] = [
     {
-        width: 200,
-        label: 'Dessert',
-        dataKey: 'dessert',
+        key: "title",
+        label: "歌曲",
+        width: 300
     },
     {
-        width: 120,
-        label: 'Calories\u00A0(g)',
-        dataKey: 'calories',
-        numeric: true,
+        key: "artists",
+        label: "歌手",
+        width: 300
     },
     {
-        width: 120,
-        label: 'Fat\u00A0(g)',
-        dataKey: 'fat',
-        numeric: true,
+        key: "duration",
+        label: "时长",
+        width: 100
     },
-    {
-        width: 120,
-        label: 'Carbs\u00A0(g)',
-        dataKey: 'carbs',
-        numeric: true,
-    },
-    {
-        width: 120,
-        label: 'Protein\u00A0(g)',
-        dataKey: 'protein',
-        numeric: true,
-    },
-];
-
-const rows: Data[] = Array.from({ length: 2000 }, (_, index) => {
-    const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-    return createData(index, ...randomSelection);
-});
+]
 
 const VirtuosoTableComponents: TableComponents<Data> = {
     Scroller: React.forwardRef<HTMLDivElement>((props, ref) => (
-        <TableContainer component={Paper} {...props} ref={ref} sx={{ borderRadius: "0px", boxShadow: "unset", background:"rgba(0,0,0,0)"}} />
+        <TableContainer component={Paper} {...props} ref={ref} sx={{ borderRadius: "0px", boxShadow: "unset", background: "rgba(0,0,0,0)" }} />
     )),
     Table: (props) => (
-        <Table {...props} sx={{  borderCollapse: 'separate', tableLayout: 'fixed' }}/>
+        <Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} />
     ),
-    TableHead,
+    TableHead: () => (<TableRow>
+        {columns.map((column) => (
+            <TableCell
+                key={column.key}
+                variant="head"
+                style={{ width: column.width }}
+                sx={{
+                    backgroundColor: 'background.paper',
+                    borderBottom: "unset"
+                }}
+            >
+                {column.label}
+            </TableCell>
+        ))}
+    </TableRow>),
     TableRow: ({ item: _item, ...props }) => <TableRow {...props} />,
     TableBody: React.forwardRef<HTMLTableSectionElement>((props, ref) => (
         <TableBody {...props} ref={ref} />
     )),
 };
 
-function fixedHeaderContent() {
-    return (
-        <TableRow>
-            {columns.map((column) => (
-                <TableCell
-                    key={column.dataKey}
-                    variant="head"
-                    align={column.numeric || false ? 'right' : 'left'}
-                    style={{ width: column.width }}
-                    sx={{
-                        backgroundColor: 'background.paper',
-                    }}
-                >
-                    {column.label}
-                </TableCell>
-            ))}
-        </TableRow>
-    );
-}
+
 
 function rowContent(_index: number, row: Data) {
     return (
         <React.Fragment>
             {columns.map((column) => (
                 <TableCell
-                    key={column.dataKey}
-                    align={column.numeric || false ? 'right' : 'left'}
+                    key={column.key}
+                    sx={{ borderBottom: "unset" }}
                 >
                     {
-                        column.dataKey === "dessert"? _index: row[column.dataKey]
+                        row[column.key]
                     }
                 </TableCell>
             ))}
@@ -148,35 +104,50 @@ interface MusicAlbumProps extends BoxProps {
 
 function MusicAlbum(props: MusicAlbumProps) {
     const router = useRouter()
-    const {id} = router.query
+    const { id } = router.query
     const [albumInfo, setAlbumInfo] = React.useState({
-        name: "album id: " + id,
-        cover: "",
+        name: "TVアニメ『ぼっち・ざ・ろっく!』フルアルバム「結束バンド」",
+        artist: "結束バンド",
+        cover: "https://oss.inkneko.com/heimusic/avatar/COVER.jpg",
+        date: undefined,
         listenedCount: 0,
     })
+
+    /**
+     * 
+     *         setMusicCoverUrl("https://oss.inkneko.com/heimusic/avatar/COVER.jpg");
+        setMusicName("星座になれたら");
+        setMusicSinger("結束バンド")
+     */
     const [viewAtTopState, setViewAtTopState] = React.useState(true)
     const AlbumInfo = (
         <Box sx={{
             marginLeft: '3%',
             marginRight: '3%',
             marginTop: '20px',
-            display: 'flex',
+            width: "auto",
             paddingBottom: '10px',
+            display: 'flex',
         }}>
             <Avatar sx={{
-                width: '180px', height: '180px', borderRadius: '3%'
+                width: '180px', height: '180px', borderRadius: '3%', flex: "0 0 auto"
             }} variant="square" src={albumInfo.cover}><MusicNote style={{ fontSize: 72 }} /></Avatar>
             <Box sx={{
                 marginLeft: '20px',
-                flex: '1',
+                marginRight: '20px',
+                width: "auto",
+                overflow: "hidden",
+                flex: "1 1 auto",
                 display: 'flex',
-                flexFlow: 'column',
+                flexDirection: 'column',
             }}>
-                <Box sx={{ marginLeft: "-2px", fontSize: '28px' }}>{albumInfo.name}</Box>
-                <Box sx={{ margin: "14px 0px", fontSize: "14px", color: "gray" }}>{"播放量 " + albumInfo.listenedCount}</Box>
+                <Typography fontWeight={600} variant='h5' noWrap >{albumInfo.name}</Typography>
+                <Typography variant='body2' noWrap >{albumInfo.artist}</Typography>
+                {albumInfo.date ? <Typography variant='caption' noWrap >{albumInfo.date}</Typography> : null}
+                <Typography variant='caption' sx={{ marginTop: "12px" }} >{"播放量 " + albumInfo.listenedCount}</Typography>
                 <Box sx={{ marginTop: "auto" }}>
-                    <Button sx={{ width: "90px", height: "32px", marginRight: "30px" }} >播放全部</Button>
-                    <Button className="album-brief-tool-bar-btn">下载</Button>
+                    <Button sx={{ width: "90px", height: "32px", marginRight: "30px" }} variant="contained" >播放全部</Button>
+                    <Button sx={{ width: "90px", height: "32px", marginRight: "30px" }} variant="outlined">下载</Button>
                 </Box>
             </Box>
         </Box>
@@ -195,11 +166,14 @@ function MusicAlbum(props: MusicAlbumProps) {
             }} variant="square" src={albumInfo.cover}><MusicNote style={{ fontSize: 72 }} /></Avatar>
             <Box sx={{
                 marginLeft: '20px',
+                width: "auto",
+                overflow: "hidden",
                 flex: '1',
                 display: 'flex',
                 flexFlow: 'column',
             }}>
-                <Box sx={{ marginLeft: "-2px", fontSize: '28px' }}>{albumInfo.name}</Box>
+                <Typography fontWeight={600} variant='h5' noWrap >{albumInfo.name}</Typography>
+
                 <Box sx={{ margin: "14px 0px", fontSize: "14px", color: "gray" }}>{"播放量 " + albumInfo.listenedCount}</Box>
             </Box>
             <Box sx={{ marginTop: "auto" }}>
@@ -211,12 +185,10 @@ function MusicAlbum(props: MusicAlbumProps) {
 
     return (
         <Box sx={{ height: '100%', width: '100%', display: "flex", flexDirection: "column", }}>
-            {/* {viewAtTopState ? AlbumInfo : AlbumInfoLite} */}
-            {AlbumInfoLite}
+            {viewAtTopState ? AlbumInfo : AlbumInfoLite}
             <TableVirtuoso
                 data={rows}
                 components={VirtuosoTableComponents}
-                fixedHeaderContent={fixedHeaderContent}
                 itemContent={rowContent}
                 atTopStateChange={(atTop) => setViewAtTopState(atTop)}
             />
