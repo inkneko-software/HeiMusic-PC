b6ae20ce1a002955801a85aafe6199b34947b1a1
leaf-lxh <meleaf@qq.com>
2023-03-07 22:39:20 +0800

    try playing music and add fade volume effect

--- renderer/components/MusicControlPannel/MusicControlPannel.tsx
+++ renderer/components/MusicControlPannel/MusicControlPannel.tsx
@@ -25,7 +25,7 @@ interface IMusicControlPannel extends BoxProps {
 function MusicControlPannel(props: IMusicControlPannel) {
     const theme = useTheme();
 
-    const [musicCoverUrl, setMusicCoverUrl] = React.useState(MusicNote.toString(),);
+    const [musicCoverUrl, setMusicCoverUrl] = React.useState<string>("undefined");
     const [musicName, setMusicName] = React.useState("HeiMusic!");
     const [musicSinger, setMusicSinger] = React.useState("author: inkneko.");
     const [playBtnIcon, setPlayBtnIcon] = React.useState(<PlayCircleFilled style={{ fontSize: 42 }} />);
@@ -44,21 +44,59 @@ function MusicControlPannel(props: IMusicControlPannel) {
 
     const volumeButtonRef = React.useRef()
     const setAudioAndPlay = () => {
-        audioRef.current.src = "https://music.inkneko.com/青空JumpingHeart.flac";
-        audioRef.current.play();
+        audioRef.current.src = "https://oss.inkneko.com/heimusic/14.%E8%BB%A2%E3%81%8C%E3%82%8B%E5%B2%A9%E3%80%81%E5%90%9B%E3%81%AB%E6%9C%9D%E3%81%8C%E9%99%8D%E3%82%8B.flac?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=HK7VZR1BIZ04M2QY2VLJ%2F20230307%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230307T140300Z&X-Amz-Expires=604800&X-Amz-Security-Token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiJISzdWWlIxQklaMDRNMlFZMlZMSiIsImV4cCI6MTY3ODIwNjMzNCwicGFyZW50IjoiaGVpbXVzaWMifQ.7b2T9L2eyHQLtPyzE1s8NV_rsf1VVyEuFsKOuGglzpNGbmjXDF-yqAl0loRZz-r88_oCi-87jdNaKeYF2PcDiQ&X-Amz-SignedHeaders=host&versionId=null&X-Amz-Signature=8f37340d7bcfc7c65eaba0a42b11221ebf8753dcae901b2dd539d9302d21a538";
+        setMusicCoverUrl("https://oss.inkneko.com/heimusic/avatar/COVER.jpg");
+        setMusicName("星座になれたら");
+        setMusicSinger("結束バンド")
     }
-
     const onPlay = () => {
+        //fixme: 按钮在事件未完成时被按下
+        var handle = null;
+        var sourceVolume = audioRef.current.volume;
+        if (handle !== null) {
+            clearInterval(handle);
+            audioRef.current.volume = sourceVolume
+        }
+
         if (audioRef.current.paused) {
-            setAudioAndPlay();
+            if (audioRef.current.src === "") {
+                setAudioAndPlay();
+            }
+            audioRef.current.play();
+            var counter = 0;
+            sourceVolume = audioRef.current.volume;
+            audioRef.current.volume = 0;
+            handle = setInterval(() => {
+                audioRef.current.volume += sourceVolume / 10
+                counter += 1;
+                if (counter === 10) {
+                    audioRef.current.volume = sourceVolume;
+                    clearInterval(handle);
+                    handle = null;
+                }
+            }, 100)
             setPlayBtnIcon(<PauseCircleFilled style={{ fontSize: 42 }} />)
 
         } else {
-            audioRef.current.pause();
+            var counter = 0;
+            sourceVolume = audioRef.current.volume;
+            handle = setInterval(() => {
+                audioRef.current.volume -= sourceVolume / 10
+                counter += 1;
+                if (counter === 10) {
+                    audioRef.current.volume = 0;
+                    clearInterval(handle);
+                    audioRef.current.pause();
+                    audioRef.current.volume = sourceVolume;
+                    handle = null;
+                }
+            }, 100)
             setPlayBtnIcon(<PlayCircleFilled style={{ fontSize: 42 }} />)
 
         }
     }
+
+
 
     React.useEffect(() => {
         if (typeof (audioRef.current) !== "undefined") {
@@ -75,10 +113,10 @@ function MusicControlPannel(props: IMusicControlPannel) {
                 setTimeLabel(`${currentMinutes}:${currentSeconds} / ${durationMinutes}:${durationSeconds}`)
             }
 
-            audio.onpause = ()=>{
+            audio.onpause = () => {
 
             }
-            
+
             return () => {
                 audio.ontimeupdate = null;
             }
@@ -89,7 +127,7 @@ function MusicControlPannel(props: IMusicControlPannel) {
     return (
         <Box sx={{ display: "flex", flexDirection: "column" }} {...props}>
             <audio ref={audioRef} />
-            <MusicSlider size="small" max={duration} value={currentTime} onChangeCommitted={(event, value: number) => { audioRef.current.currentTime = value; audioRef.current.play() }} />
+            <MusicSlider size="small" max={duration} value={currentTime} onChangeCommitted={(event, value: number) => { audioRef.current.currentTime = value; audioRef.current.play(); setCurrentTime(value) }} />
             <Box sx={{ display: "flex", margin: "auto 0px", flexGrow: "1" }}>
                 <Box sx={{
                     display: "flex", marginLeft: "20px", width: "30%", textAlign: "left"
@@ -100,10 +138,10 @@ function MusicControlPannel(props: IMusicControlPannel) {
                         <Box sx={{ fontSize: "12px", color: "#a1a1a1", overflow: "hidden", textOverflow: "ellipsis" }}>{musicSinger}</Box>
                     </Box>
                 </Box>
-                <Box sx={{ flexGrow: "1", margin:"auto auto", textAlign:"center"}} >
+                <Box sx={{ flexGrow: "1", margin: "auto auto", textAlign: "center" }} >
                     <IconButton sx={{ color: theme.palette.text.primary }}><Repeat style={{ fontSize: 24 }} /></IconButton>
-                    <IconButton sx={{ backgroundColor: "transparent", color: theme.palette.text.primary }} ><SkipPrevious style={{ fontSize: 34 }} /></IconButton>
-                    <IconButton sx={{ color: theme.palette.primary.main }} onClick={() => { onPlay() }} >{playBtnIcon}</IconButton>
+                    <IconButton sx={{ color: theme.palette.text.primary }} ><SkipPrevious style={{ fontSize: 34 }} /></IconButton>
+                    <IconButton color="primary" onClick={() => { onPlay() }} >{playBtnIcon}</IconButton>
                     <IconButton sx={{ backgroundColor: "transparent", color: theme.palette.text.primary }} ><SkipNext style={{ fontSize: 34 }} /></IconButton>
                     <VolumePannel
                         open={volumePanelOpen}
