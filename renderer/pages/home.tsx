e4abb931de10751024d926d2fea6b552b20f255b
leaf-lxh <meleaf@qq.com>
2024-01-01 21:10:24 +0800

    优化比例对其方式

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -21,20 +21,20 @@ function AlbumCard(props: IAlbumCard) {
     const [width, setWidth] = React.useState(0);
     const [loaded, setLoaded] = React.useState(false)
 
-    React.useEffect(() => {
-        if (gridRef.current !== null) {
-            setWidth(gridRef.current.clientWidth - 24)
-            // 性能过低
-            const observer = new ResizeObserver(()=>{
-                setWidth(gridRef.current.clientWidth - 24)
-                console.log(gridRef.current.clientWidth - 24)
-            });
-            observer.observe(gridRef.current)
-            return ()=>{
-                observer.disconnect();
-            }
-        }
-    }, [gridRef])
+    // React.useEffect(() => {
+    //     if (gridRef.current !== null) {
+    //         setWidth(gridRef.current.clientWidth - 24)
+    //         // 性能过低
+    //         const observer = new ResizeObserver(()=>{
+    //             setWidth(gridRef.current.clientWidth - 24)
+    //             console.log(gridRef.current.clientWidth - 24)
+    //         });
+    //         observer.observe(gridRef.current)
+    //         return ()=>{
+    //             observer.disconnect();
+    //         }
+    //     }
+    // }, [gridRef])
 
     React.useEffect(() => {
         if (imgRef.current !== null) {
@@ -57,8 +57,13 @@ function AlbumCard(props: IAlbumCard) {
 
         <Link href={`/album/${props.album.albumId}`}>
             <Grid ref={gridRef} item xs={3} sx={{ display: "flex", flexDirection: 'column', flexShrink: "0" }}>
-                <Skeleton variant="rounded" sx={[loaded && { display: 'none' }, { width: width, height: width }]} />
-                <CardMedia ref={imgRef} sx={[!loaded && { display: 'none' }, { borderRadius: "6%", border: "1px solid #e3e3e3" ,width: width, height: width, objectFit:"contain" }]} component='img' ></CardMedia>
+                <Box sx={[{ borderRadius: "6%", display: "flex", aspectRatio: '1 / 1' }, loaded && { display: 'none' },]}>
+                    <Skeleton variant="rounded" sx={[{ width: '100%', height: '100%' }]} />
+                </Box>
+                <Box sx={[{ borderRadius: "6%", border: "1px solid #e3e3e3", aspectRatio: '1 / 1', display: "flex", overflow: 'hidden' }, !loaded && { display: 'none' },]}>
+                    <CardMedia ref={imgRef} sx={[{ margin: "auto auto", objectFit: "contain" }]} component='img' ></CardMedia>
+                </Box>
+
                 <Typography
                     variant="subtitle2"
                     sx={{
@@ -88,17 +93,17 @@ function Home() {
         AlbumControllerService.getRecentUpload()
             .then(res => {
                 setRecentUploadAlbum(res.data)
-                if (res.data.length === 0){
+                if (res.data.length === 0) {
                     setFirstLaunch(true);
                 }
             })
-            .catch(error=>{
+            .catch(error => {
                 makeToast("网络连接失败", "error", "bottom-right")
             })
     }, [])
 
     return (
-        <Box sx={{ width: 'auto', height: "100%", padding: "12px 12px", overflowY: 'auto', overflowX: 'hidden'}}>
+        <Box sx={{ width: 'auto', height: "100%", padding: "12px 12px", overflowY: 'auto', overflowX: 'hidden' }}>
             {Toast}
             {/* <Box>
                 <Typography variant='h5' sx={{ marginBottom: "12px" }}>热门歌单</Typography>
@@ -111,14 +116,14 @@ function Home() {
                 </Grid>
             </Box> */}
             <Box sx={[{ display: "none" }, firstLaunch && { display: "flex", flexDirection: "column" }]}>
-                <Typography sx={{marginBottom: "24px", userSelect: "none"}}>欢迎使用HeiMusic!</Typography>
-                <Typography sx={{marginBottom: "24px", userSelect: "none"}}>第一次使用请设置<Link href="/init"><MuiLink>管理账户</MuiLink></Link></Typography>
-                <Typography sx={{marginBottom: "24px", userSelect: "none"}}>设置完成后，可通过左侧专辑管理按钮进行音乐导入</Typography>
+                <Typography sx={{ marginBottom: "24px", userSelect: "none" }}>欢迎使用HeiMusic!</Typography>
+                <Typography sx={{ marginBottom: "24px", userSelect: "none" }}>第一次使用请设置<Link href="/init"><MuiLink>管理账户</MuiLink></Link></Typography>
+                <Typography sx={{ marginBottom: "24px", userSelect: "none" }}>设置完成后，可通过左侧专辑管理按钮进行音乐导入</Typography>
             </Box>
             <Box sx={[firstLaunch && { display: "none" }]}>
                 <Typography variant='h5' sx={{ marginBottom: "12px" }}>最新上传</Typography>
                 <Typography sx={[recentUploadAlbum.length !== 0 && { display: "none" }]}>暂无音乐</Typography>
-                <Grid container spacing={3} sx={{ display: 'flex', justifyContent: "flex-start" }} columns={{ xs: 12, lg: 15, xl: 18 }} >
+                <Grid container spacing={3} sx={{ display: 'flex', justifyContent: "flex-start" }} columns={{ xs: 12, lg: 15, xl: 15 }} >
                     {
                         recentUploadAlbum.map((album, index) => {
                             return <AlbumCard key={index} album={album} />
