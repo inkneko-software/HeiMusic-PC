b86c9a4220abe5105c2126c03fb1c9630ffe0f4b
leaf-lxh <meleaf@qq.com>
2024-01-06 19:11:40 +0800

    添加操作菜单，可直接播放专辑

--- renderer/pages/album/management.tsx
+++ renderer/pages/album/management.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Stack, Typography, Avatar, IconButton } from "@mui/material";
+import { Box, Button, Stack, Typography, Avatar, IconButton, Popover, PopoverProps, Divider, Dialog, DialogActions, DialogTitle, DialogContent } from "@mui/material";
 
 import Link from "next/link";
 import React from "react"
@@ -18,7 +18,13 @@ import { AlbumControllerService, AlbumVo, ApiError } from "@api/codegen";
 import { pushToast } from "@components/HeiMusicMainLayout";
 import ImageSkeleton from "@components/Common/ImageSkeleton";
 import { useRouter } from "next/router";
-
+import MusicVideoOutlinedIcon from '@mui/icons-material/MusicVideoOutlined';
+import LibraryMusicOutlinedIcon from '@mui/icons-material/LibraryMusicOutlined';
+import EditNoteOutlinedIcon from '@mui/icons-material/EditNoteOutlined';
+import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { IChangePlayListEvent } from "@components/MusicControlPannel/MusicControlPannel";
 function AlbumManagement() {
     const router = useRouter();
     const theme = useTheme()
@@ -28,6 +34,13 @@ function AlbumManagement() {
     const [albumList, setAlbumList] = React.useState<AlbumVo[]>([]);
 
     const tableRef = React.useRef<HTMLDivElement>(null);
+
+    //专辑右键菜单
+    const [albumMenuOpen, setAlbumMenuOpen] = React.useState(false);
+    const [albumMenuPos, setAlbumMenuPos] = React.useState({ top: 0, left: 0 })
+    const [albumMenuInfo, setAlbumMenuInfo] = React.useState({ title: "", albumId: 0 })
+    //删除专辑确认对话框
+    const [deleteAlbumConfirmDialogOpen, setDeleteAlbumConfirmDialogOpen] = React.useState(false);
     React.useEffect(() => {
         const { p, s } = router.query;
         var page: number;
@@ -60,36 +73,65 @@ function AlbumManagement() {
     const handleChangePage = (event: unknown, newPage: number) => {
         //分页组件的起始页为0，但后端的起始页为1，手动修正
         router.push(`/album/management?p=${newPage + 1}&s=${rowsPerPage}`)
-        //tableRef.current.scrollTo({left:0, top: 0});
-        //setPage(newPage);
-        // AlbumControllerService.getAlbumList(newPage + 1, rowsPerPage)
-        //     .then(res => {
-        //         setTotal(res.data.total);
-        //         setAlbumList(res.data.albumList);
-        //     })
-        //     .catch((error: ApiError)=>{
-        //         pushToast(error.message)
-        //     })
     };
 
     const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
         //tableRef.current.scrollTo({left:0, top: 0});
         var newPageRange: number = parseInt(event.target.value);
         router.push(`/album/management?p=${1}&s=${newPageRange}`)
-
-        // setRowsPerPage(newPageRange);
-        // setPage(0);
-        // AlbumControllerService.getAlbumList(1, newPageRange)
-        //     .then(res => {
-        //         setTotal(res.data.total);
-        //         setAlbumList(res.data.albumList);
-        //     })
-        //     .catch((error: ApiError)=>{
-        //         pushToast(error.message)
-        //     })
     };
 
+    const handleMenuPlayAlbum = (albumId: number) => {
+        (async () => {
+            try {
+                var albumInfo = (await AlbumControllerService.getAlbum(albumId)).data;
 
+                var playlist = (await AlbumControllerService.getAlbumMusicList(albumId)).data.map((music, index) => {
+                    return {
+                        musicId: music.musicId,
+                        title: music.title,
+                        artists: music.artistList.map(val => val.name),
+                        qualityOption: [{
+                            name: "SQ",
+                            url: music.resourceUrl,
+                            color: "red"
+                        }],
+                        albumId: albumInfo.albumId,
+                        albumTitle: albumInfo.title,
+                        cover: albumInfo.frontCoverUrl,
+                        duration: music.duration,
+                        isFavorite: music.isFavorite
+                    }
+                });
+
+                if (playlist.length !== 0) {
+                    const event = new CustomEvent<IChangePlayListEvent>("music-control-panel::changePlayList", {
+                        detail: {
+                            playlist: playlist,
+                            startIndex: 0
+                        }
+                    });
+                    document.dispatchEvent(event)
+                }
+
+            } catch (error) {
+                var apiError = error as ApiError;
+                pushToast(apiError.message, 'error');
+            }
+        })()
+
+    }
+
+    const handleDeleteAlbum = async function () {
+        try {
+            await AlbumControllerService.removeAlbum(albumMenuInfo.albumId)
+            router.reload();
+
+        } catch (e) {
+            pushToast(e.message)
+        }
+
+    }
 
     return (
         <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}  >
@@ -115,19 +157,81 @@ function AlbumManagement() {
                 </Stack>
 
             </Box>
+            {/* 删除专辑确认对话框 */}
+            <Dialog open={deleteAlbumConfirmDialogOpen} onClose={() => setDeleteAlbumConfirmDialogOpen(false)}>
+                <DialogTitle>确认删除当前专辑？</DialogTitle>
+                <DialogContent>
+                    <Typography>专辑标题：{albumMenuInfo.title}</Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleDeleteAlbum}>确认</Button>
+                    <Button onClick={() => setDeleteAlbumConfirmDialogOpen(false)}>取消</Button>
+                </DialogActions>
+            </Dialog>
+            {/* 专辑菜单 */}
+            <Popover
+                open={albumMenuOpen}
+                anchorReference='anchorPosition'
+                anchorPosition={albumMenuPos}
+                onClose={() => setAlbumMenuOpen(false)}
+                transitionDuration={0}
+            >
+                <Paper sx={{ display: 'flex', flexDirection: 'column', width: "100px", backgroundColor: theme.palette.pannelBackground.main }}
+                    onClick={() => setAlbumMenuOpen(false)}
+                >
+                    <Button
+                        sx={{ justifyContent: 'flex-start', padding: "6px 16px" }}
+                        color='inherit'
+                        startIcon={<PlayCircleFilledWhiteOutlinedIcon />}
+                        onClick={() => handleMenuPlayAlbum(albumMenuInfo.albumId)}
+                    >
+                        播放
+                    </Button>
+                    <Button
+                        sx={{ justifyContent: 'flex-start', padding: "6px 16px" }}
+                        color='inherit'
+                        startIcon={<LibraryMusicOutlinedIcon />}
+                        onClick={() => { router.push(`/album/${albumMenuInfo.albumId}`) }}
+                    >
+                        查看
+                    </Button>
+                    <Divider />
+                    <Button
+                        sx={{ justifyContent: 'flex-start', padding: "6px 16px" }}
+                        color='inherit'
+                        startIcon={<EditNoteOutlinedIcon />}
+                        onClick={() => { router.push(`/album/edit/${albumMenuInfo.albumId}`) }}
+                    >
+                        编辑
+                    </Button>
+                    <Button
+                        sx={{ justifyContent: 'flex-start', padding: "6px 16px" }}
+                        color='inherit'
+                        startIcon={<DeleteForeverIcon />}
+                        onClick={() => setDeleteAlbumConfirmDialogOpen(true)}
+                    >
+                        删除
+                    </Button>
+                </Paper>
+            </Popover>
+            {/* 表头 */}
             <TableRow sx={{ display: "table" }}>
                 <TableCell width="15%" >专辑封面</TableCell>
                 <TableCell width="35%" >专辑标题</TableCell>
                 <TableCell width="30%">艺术家</TableCell>
-                <TableCell width="10%" >歌曲数</TableCell>
+                <TableCell width="10%" >曲数</TableCell>
                 <TableCell width="10%" >操作</TableCell>
             </TableRow>
+            {/* 表格 */}
             <TableContainer sx={{ overflowY: "auto", overflowX: "hidden", width: "auto" }} ref={tableRef}>
-                <Table sx={{ tableLayout: 'fixed', margin: "0px 6px", ".MuiTableCell-root": { padding: "12px 6px" } }} >
+                <Table sx={{ tableLayout: 'fixed', margin: "0px 6px", ".MuiTableCell-root": { padding: "10px 16px" } }} >
                     <TableBody >
                         {
                             albumList.map((album, index) => (
-                                <TableRow key={album.albumId} >
+                                <TableRow
+                                    key={album.albumId}
+                                    sx={{ ':hover': { background: theme.palette.pannelBackground.main } }}
+                                >
                                     <TableCell sx={{ width: "15%", borderBottom: "unset" }} align="center">
                                         <ImageSkeleton sx={{
                                             width: '32px', height: '32px', borderRadius: '6%', flex: "0 0 auto", marginLeft: "12px", imageRendering: "auto", objectFit: "contain"
@@ -147,7 +251,19 @@ function AlbumManagement() {
                                     </TableCell>
                                     <TableCell sx={{ width: "10%", borderBottom: "unset" }}>{album.musicNum}</TableCell>
                                     <TableCell sx={{ width: "10%", borderBottom: "unset" }}>
-                                        <Link href={`/album/edit/${album.albumId}`}><Button variant="outlined" size="small">编辑</Button></Link>
+                                        <IconButton
+                                            size='small'
+                                            sx={{ padding: '0 0' }}
+                                            onClick={e => {
+                                                setAlbumMenuOpen(true);
+                                                setAlbumMenuPos({ left: e.clientX, top: e.clientY });
+                                                setAlbumMenuInfo({ title: album.title, albumId: album.albumId });
+                                            }}
+                                        >
+                                            <MoreHorizIcon />
+                                        </IconButton>
+
+
                                     </TableCell>
                                 </TableRow>
                             ))
