515adba9c2ea10fd720997ad743645ddecbf3b24
leaf-lxh <meleaf@qq.com>
2024-03-28 11:54:13 +0800

    优化交互效果、添加对largeTrack的支持

--- renderer/pages/search/index.tsx
+++ renderer/pages/search/index.tsx
@@ -1,12 +1,15 @@
 import { ApiError, MusicVo, SearchControllerService } from "@api/codegen";
 import { pushToast } from "@components/HeiMusicMainLayout";
-import { IChangePlayListEvent, IEnqueueNextEvent } from "@components/MusicControlPannel/MusicControlPannel";
-import { Table, TableBody, TableCell, TableHead, TableRow, Box, Typography } from "@mui/material";
+import { IChangePlayListEvent, IEnqueueNextEvent, IMusicInfo } from "@components/MusicControlPannel/MusicControlPannel";
+import { Table, TableBody, TableCell, TableHead, TableRow, Box, Typography, TableContainer } from "@mui/material";
+import { useTheme } from "@mui/styles";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react"
 
 export default function Search() {
-    const [result, setResult] = React.useState<MusicVo[]>([]);
+    const theme = useTheme();
+    const [result, setResult] = React.useState<MusicVo[]>(null);
     const [prompt, setPrompt] = React.useState("");
     const router = useRouter();
 
@@ -25,7 +28,7 @@ export default function Search() {
     }, [router.query])
 
     const handlePlay = (music: MusicVo) => {
-        var musicList = [{
+        var musicList: IMusicInfo[] = [{
             musicId: music.musicId,
             title: music.title,
             artists: music.artistList.map(val => val.name),
@@ -38,7 +41,10 @@ export default function Search() {
             albumTitle: music.albumTitle,
             cover: music.albumCoverUrl,
             duration: music.duration,
-            isFavorite: music.isFavorite
+            isFavorite: music.isFavorite,
+            isLargeTrackMusic: music.discStartTime !== '',
+            discEndTime: parseFloat(music.discEndTime),
+            discStartTime: parseFloat(music.discStartTime)
         }]
         const event = new CustomEvent<IChangePlayListEvent>("music-control-panel::changePlayList", { detail: { playlist: musicList, startIndex: 0 } });
 
@@ -46,40 +52,51 @@ export default function Search() {
     }
 
     return (
-        <Box sx={{ width: '100%', height: '100%', padding: '12px 12px', overflowY: 'auto', overflowX: 'hidden', display: 'flex', flexDirection: 'column' }}>
-            <Typography variant="h5">{`搜索：${prompt}`}</Typography>
-            <Table>
-                <TableHead>
-                    <TableCell>
-                        歌曲
-                    </TableCell>
-                    <TableCell>
-                        艺术家
-                    </TableCell>
-                    <TableCell>
-                        专辑
-                    </TableCell>
-                </TableHead>
-                <TableBody>
-                    {
-                        result.map(music => {
-                            return (
-                                <TableRow onDoubleClick={() => handlePlay(music)} >
-                                    <TableCell>
-                                        <Typography variant='body2' noWrap title={music.title}>{music.title}</Typography>
-                                    </TableCell>
-                                    <TableCell>
-                                        <Typography variant='body2' noWrap title={music.artistList.map(artist => artist.name).join(' / ')}>{music.artistList.map(artist => artist.name).join(' / ')}</Typography>
-                                    </TableCell>
-                                    <TableCell>
-                                        <Typography variant='body2' noWrap title={music.albumTitle}>{music.albumTitle}</Typography>
-                                    </TableCell>
-                                </TableRow>
-                            )
-                        })
-                    }
-                </TableBody>
-            </Table>
+        <Box sx={{ width: '100%', height: '100%', overflowY: 'auto', overflowX: 'hidden', display: 'flex', flexDirection: 'column' }}>
+            <Typography variant="h5" sx={{ padding: '0px 12px' }}>{`搜索音乐：${prompt}`}</Typography>
+            <Typography variant='caption' sx={{ padding: '0px 12px' }} >{result === null ? "加载中" : `共${result.length}个结果`}</Typography>
+            {
+                result !== null && result.length !== 0 &&
+                <TableContainer sx={{ padding: '0px 12px' }}>
+                <Table sx={{ tableLayout: "fixed", }} stickyHeader >
+                    <TableHead>
+                        <TableCell sx={{ backgroundColor: theme.palette.pannelBackground.light }}>
+                            歌曲
+                        </TableCell >
+                        <TableCell sx={{ backgroundColor: theme.palette.pannelBackground.light }}>
+                            艺术家
+                        </TableCell>
+                        <TableCell sx={{ backgroundColor: theme.palette.pannelBackground.light }}>
+                            专辑
+                        </TableCell>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            result.map(music => {
+                                return (
+                                    <TableRow
+                                        onDoubleClick={() => handlePlay(music)}
+                                        sx={[{ ':hover': { background: theme.palette.pannelBackground.main } }/*, albumMenuOpen && albumMenuInfo.albumId === album.albumId && { background: theme.palette.pannelBackground.main }*/]}>
+                                        <TableCell sx={{ width: '30%' }}>
+                                            <Typography variant='body2' noWrap title={music.title} sx={{ ':hover': { cursor: 'default', color: theme.palette.primary.main } }}>{music.title}</Typography>
+                                        </TableCell>
+                                        <TableCell sx={{ width: '30%' }}>
+                                            <Typography variant='body2' noWrap sx={{ ':hover': { cursor: 'pointer', color: theme.palette.primary.main } }} title={music.artistList.map(artist => artist.name).join(' / ')}>{music.artistList.map(artist => artist.name).join(' / ')}</Typography>
+                                        </TableCell>
+                                        <TableCell sx={{ width: '40%' }}>
+                                            <Link href={`/album/${music.albumId}`}>
+                                                <Typography variant='body2' noWrap title={music.albumTitle} sx={{ ':hover': { cursor: 'pointer', color: theme.palette.primary.main } }} >{music.albumTitle}</Typography>
+                                            </Link>
+                                        </TableCell>
+                                    </TableRow>
+                                )
+                            })
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            }
+
         </Box>
 
     )
