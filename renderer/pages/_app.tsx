dd683dc45aeb1b78e1a12206d5f9f27d2a980e5f
leaf-lxh <meleaf@qq.com>
2024-05-13 00:04:12 +0800

    设定仅当登录后才可访问站点

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactElement, ReactNode } from 'react';
 import Head from 'next/head';
 import { ThemeProvider, Theme, StyledEngineProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -6,35 +6,51 @@ import type { AppProps } from 'next/app';
 import HeiMusicThemeProvider from '../lib/HeiMusicThemeProvider';
 import HeiMusicMainLayout from '../components/HeiMusicMainLayout';
 import { ICurrentMusicInfo, HeiMusicContext } from '../lib/HeiMusicContext';
+import { ApiError, UserControllerService } from '@api/codegen';
+import Login from './login';
+import { useRouter } from 'next/router';
+import { NextPage } from 'next';
 
 
 declare module '@mui/styles/defaultTheme' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme { }
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface DefaultTheme extends Theme { }
+}
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+    getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
 }
 
 
-
 export default function (props: AppProps) {
-  const { Component, pageProps } = props;
+    const { Component, pageProps }: AppPropsWithLayout = props;
+    const [noLogin, setNoLogin] = React.useState(true);
+    const [currentMusicInfo, setCurrentMusicInfo] = React.useState<ICurrentMusicInfo>(null);
+    const router = useRouter();
 
-  const [currentMusicInfo, setCurrentMusicInfo] = React.useState<ICurrentMusicInfo>(null);
+    React.useEffect(() => {
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
 
-  React.useEffect(() => {
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
+    const handleLoginSuccess = () => {
+        setNoLogin(false);
     }
-  }, []);
 
-  return (
-    <React.Fragment>
-      <Head>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <title>HeiMusic</title>
-        <style>
-          {
-            `::-webkit-scrollbar {
+    return (
+        <React.Fragment>
+            <Head>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+                <title>HeiMusic</title>
+                <style>
+                    {
+                        `::-webkit-scrollbar {
                 width: 12px;
                 height: 8px;
                 background-color: #e3e3e3; /* or add it to the track */
@@ -76,20 +92,29 @@ export default function (props: AppProps) {
                 height: 100%;
               }
               `
-          }
+                    }
 
-        </style>
-      </Head>
-      <StyledEngineProvider injectFirst>
-        <HeiMusicThemeProvider >
-          <HeiMusicContext.Provider value={{currentMusicInfo: currentMusicInfo, setCurrentMusicInfo: setCurrentMusicInfo}}>
-            <CssBaseline />
-            <HeiMusicMainLayout>
-              <Component {...pageProps} />
-            </HeiMusicMainLayout>
-          </HeiMusicContext.Provider>
-        </HeiMusicThemeProvider>
-      </StyledEngineProvider>
-    </React.Fragment>
-  );
+                </style>
+            </Head>
+            <StyledEngineProvider injectFirst>
+                <HeiMusicThemeProvider >
+                    <HeiMusicContext.Provider value={{ currentMusicInfo: currentMusicInfo, setCurrentMusicInfo: setCurrentMusicInfo }}>
+                        <CssBaseline />
+                        {
+                            noLogin && <Login onLoginSuccess={handleLoginSuccess}/>
+                        }
+                        {
+                            !noLogin && Component.getLayout && Component.getLayout(<Component {...pageProps} />)
+                        }
+                        {
+                            !noLogin && !Component.getLayout &&
+                            <HeiMusicMainLayout>
+                                <Component {...pageProps} />
+                            </HeiMusicMainLayout>
+                        }
+                    </HeiMusicContext.Provider>
+                </HeiMusicThemeProvider>
+            </StyledEngineProvider>
+        </React.Fragment>
+    );
 }
